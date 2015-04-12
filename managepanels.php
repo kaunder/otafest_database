4450246318e098d48747811b7c1be85a207ed69e
kaunder <kaunder@ucalgary.ca>
2015-04-11 23:39:44 -0600

    Insert New Panel now fully functional

--- managepanels.php
+++ managepanels.php
@@ -3,7 +3,7 @@
 <?php
    include "dashboard.php";
 ?>
-<!-- Contests specific stuff starts here-->
+<!-- Panel specific stuff starts here-->
 	<div class="col-sm-9 col-md-10 main">
 	<h1 class="page-header">Panels</h1>
  
